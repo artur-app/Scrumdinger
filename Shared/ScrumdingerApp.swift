7127c555e64896bde2e0fcad546ce68809696ba4
Artur Akhmetshin <mail@mb-training.ru>
2020-12-21 22:01:00 +0300

    Commit, first stage

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Artur Akhmetshin on 20.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
